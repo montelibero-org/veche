03eaa505a2efa7d3a0e180598a60c3abc2232cbc
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-02-03 23:02:48 +0300

    Add common.css

--- veche/src/Foundation.hs
+++ veche/src/Foundation.hs
@@ -153,6 +153,7 @@ instance Yesod App where
         pc <-
             widgetToPageContent $ do
                 addStylesheet $ StaticR css_bootstrap_css
+                addStylesheet $ StaticR css_common_css
                 $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
