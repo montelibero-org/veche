7523ca3782edcccfac603f19648784afa3ddf6cd
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-02-08 17:50:47 +0300

    Allow testnet signatures to authenticate

--- veche/test/Auth/StellarSpec.hs
+++ veche/test/Auth/StellarSpec.hs
@@ -33,25 +33,11 @@ spec = withApp do
 
         describe "authentication with tx-response" do
 
-            it "authenticates when correct tx-response" do
-                request do
-                    setMethod "POST"
-                    setUrl $ AuthR $ PluginR "stellar" []
-                    addPostParam "response" testSignedTx
-                statusIs 303 -- okay redirect
+            it "authenticates when correct tx-response (mainnet)" $
+                testAuthenticationOk testTxSignedForMainnet
 
-                get UserR
-                statusIs 200 -- authenticated
-
-                users <- runDB $ fmap entityVal <$> selectList [] []
-                assertEq
-                    "users after auth"
-                    users
-                    [ User
-                        { userName = Nothing
-                        , userStellarAddress = testPublicKey
-                        }
-                    ]
+            it "authenticates when correct tx-response (testnet)" $
+                testAuthenticationOk testTxSignedForTestnet
 
             it "doesn't authenticate when incorrect tx-response" do
                 request do
@@ -59,6 +45,27 @@ spec = withApp do
                     setUrl $ AuthR $ PluginR "stellar" []
                     addPostParam "response" testUnsingedTx
                 statusIs 400
+
+testAuthenticationOk :: Text -> YesodExample App ()
+testAuthenticationOk tx = do
+    request do
+        setMethod "POST"
+        setUrl $ AuthR $ PluginR "stellar" []
+        addPostParam "response" tx
+    statusIs 303 -- okay redirect
+
+    get UserR
+    statusIs 200 -- authenticated
+
+    users <- runDB $ fmap entityVal <$> selectList [] []
+    assertEq
+        "users after auth"
+        users
+        [ User
+            { userName = Nothing
+            , userStellarAddress = testPublicKey
+            }
+        ]
 
 testPublicKey :: Text
 testPublicKey = "GDLNZNS3HM3I3OAQKYV5WBACXCUU7JWEQLGGCAEV7E4LA4BY2XFOLEWX"
@@ -70,9 +77,16 @@ testUnsingedTx =
     "AAAAAgAAAADW3LZbOzaNuBBWK9sEAripT6bEgsxhAJX5OLBwONXK5QAAAAAAAAAA\
     \AAAAAQAAAAAAAAABAAAAEkxvZ2dpbmcgaW50byBWZWNoZQAAAAAAAAAAAAAAAAAA"
 
-testSignedTx :: Text
-testSignedTx =
+testTxSignedForMainnet :: Text
+testTxSignedForMainnet =
     "AAAAAgAAAADW3LZbOzaNuBBWK9sEAripT6bEgsxhAJX5OLBwONXK5QAAAAAAAAAA\
     \AAAAAQAAAAAAAAABAAAAEkxvZ2dpbmcgaW50byBWZWNoZQAAAAAAAAAAAAAAAAAB\
     \ONXK5QAAAEAy13Yo6c30JzWbUp24b43kCtUitlwbtijOhmU0H6cZlFm8xCa2Y9Eu\
     \WvCG0Rima5rIXHTJnPKHJdI6boKpODwM"
+
+testTxSignedForTestnet :: Text
+testTxSignedForTestnet =
+    "AAAAAgAAAADW3LZbOzaNuBBWK9sEAripT6bEgsxhAJX5OLBwONXK5QAAAAAAAAAA\
+    \AAAAAQAAAAAAAAABAAAAEkxvZ2dpbmcgaW50byBWZWNoZQAAAAAAAAAAAAAAAAAB\
+    \ONXK5QAAAEDxIVAlHINvvv9/BcFP6kvXd0Tw/F1w8jEFxVf8UISvDxw6SOmt3Z6B\
+    \Nm/CMBwyBtYXYooKhoKW6ky8A0hStCMH"
--- yesod-auth-stellar/Yesod/Auth/verify.py
+++ yesod-auth-stellar/Yesod/Auth/verify.py
@@ -1,19 +1,33 @@
 from stellar_sdk import *
 from sys import *
 
-try:
-    envelope = (
-        TransactionEnvelope
-        .from_xdr(
-            '#{envelope}',
-            'Public Global Stellar Network ; September 2015'
-        )
-    )
-    account_id = envelope.transaction.source.account_id
-    keypair = Keypair.from_public_key(account_id)
-    [signature] = envelope.signatures
-    keypair.verify(envelope.hash(), signature.signature)
-    print(account_id)
-except Exception as e:
-    print(e, file=stderr)
-    exit(1)
+
+XDR = '#{envelope}'
+
+
+MAINNET_PASSPHRASE = 'Public Global Stellar Network ; September 2015'
+TESTNET_PASSPHRASE = 'Test SDF Network ; September 2015'
+
+
+def verify(xdr, passphrase):
+    try:
+        envelope = TransactionEnvelope.from_xdr(xdr, passphrase)
+        account_id = envelope.transaction.source.account_id
+        keypair = Keypair.from_public_key(account_id)
+        [signature] = envelope.signatures
+        keypair.verify(envelope.hash(), signature.signature)
+        return (True, account_id)
+    except Exception as e:
+        return (False, str(e))
+
+
+ok, result = verify(XDR, MAINNET_PASSPHRASE)
+if ok:
+    print(result)
+else:
+    ok, result = verify(XDR, TESTNET_PASSPHRASE)
+    if ok:
+        print(result)
+    else:
+        print(result, file=stderr)
+        exit(1)
