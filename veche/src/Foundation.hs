522a5f8bbebf9cc49b437cf99ebd20d172740267
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-02-11 01:18:15 +0300

    fixup! Add login link for non-logged users

--- veche/src/Foundation.hs
+++ veche/src/Foundation.hs
@@ -346,6 +346,7 @@ getEntity404 ::
     Key val -> ReaderT backend m (Entity val)
 getEntity404 key = Entity key <$> get404 key
 
+isAuthR :: Route App -> Bool
 isAuthR = \case
     AuthR _ -> True
     _ -> False
