6b78ab0018acd3a07caa623a7ca953ff3ad03ced
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-02-10 23:37:15 +0300

    Change Bootstrap theme to the default

--- veche/src/Foundation.hs
+++ veche/src/Foundation.hs
@@ -150,6 +150,7 @@ instance Yesod App where
         pc <-
             widgetToPageContent $ do
                 addStylesheet $ StaticR css_bootstrap_css
+                addStylesheet $ StaticR css_bootstrap_theme_css
                 addStylesheet $ StaticR css_common_css
                 $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
