10f906712c56b4a43c8cd8016faa1606667f174f
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-02-20 18:56:30 +0300

    Style

--- veche/src/Workers/StellarUpdate.hs
+++ veche/src/Workers/StellarUpdate.hs
@@ -31,71 +31,69 @@ stellarDataUpdater :: BaseUrl -> ConnectionPool -> IO ()
 stellarDataUpdater baseUrl connPool =
     forever do
         putStrLn "stellarDataUpdater: Updating MTL signers"
-        n <- updateMtlSignersCache mtlFund
+        n <- updateMtlSignersCache baseUrl connPool mtlFund
         putStrLn $ "stellarDataUpdater: Updated " <> tshow n <> " items"
         randomDelay
   where
-
-    updateMtlSignersCache target = do
-        Account{signers} <- getAccount' target
-        let actual =
-                Map.fromList
-                    [ (key, weight)
-                    | Signer{key, weight, type_ = Ed25519PublicKey} <- signers
-                    , weight > 0
-                    ]
-        db do
-            cached' <- selectList [StellarSignerTarget ==. target] []
-            let cached =
-                    Map.fromList
-                        [ (stellarSignerKey, stellarSignerWeight)
-                        | Entity{entityVal} <- cached'
-                        , let
-                            StellarSigner
-                                {stellarSignerKey, stellarSignerWeight} =
-                                    entityVal
-                        ]
-            handleDeleted $ Map.keys $ cached \\ actual
-            handleAdded $ actual \\ cached
-            handleModified
-                [ (key, weightActual)
-                | (key, (weightCached, weightActual)) <-
-                    Map.assocs $ Map.intersectionWith (,) cached actual
-                , weightCached /= weightActual
-                ]
-            unless (cached == actual)
-                updateAllIssueApprovals
-        pure $ length actual
-      where
-
-        handleDeleted = traverse_ $ deleteBy . UniqueMember target
-
-        handleAdded added =
-            insertMany_
-                [ StellarSigner
-                    { stellarSignerTarget = target
-                    , stellarSignerKey
-                    , stellarSignerWeight
-                    }
-                | (stellarSignerKey, stellarSignerWeight) <- Map.assocs added
-                ]
-
-        handleModified =
-            traverse_ \(key, weight) ->
-                updateWhere
-                    [StellarSignerTarget ==. target, StellarSignerKey ==. key]
-                    [StellarSignerWeight =. weight]
-
-    getAccount' address = do
-        manager <- newTlsManager
-        eResult <- runClientM (getAccount address) $ mkClientEnv manager baseUrl
-        case eResult of
-            Left err -> throwIO err
-            Right result -> pure result
-
     randomDelay = do
         delayMinutes <- randomRIO (1, 10)
         threadDelay $ delayMinutes * 60 * 1_000_000
+
+updateMtlSignersCache ::
+    BaseUrl ->
+    ConnectionPool ->
+    Text ->
+    -- | Actual number of signers
+    IO Int
+updateMtlSignersCache baseUrl connPool target = do
+    Account{signers} <- getAccount' baseUrl target
+    let actual =
+            Map.fromList
+                [ (key, weight)
+                | Signer{key, weight, type_ = Ed25519PublicKey} <- signers
+                , weight > 0
+                ]
+    db do
+        cached' <- selectList [StellarSignerTarget ==. target] []
+        let cached =
+                Map.fromList
+                    [ (stellarSignerKey, stellarSignerWeight)
+                    | Entity{entityVal} <- cached'
+                    , let
+                        StellarSigner
+                            {stellarSignerKey, stellarSignerWeight} =
+                                entityVal
+                    ]
+        handleDeleted $ Map.keys $ cached \\ actual
+        handleAdded $ actual \\ cached
+        handleModified
+            [ (key, weightActual)
+            | (key, (weightCached, weightActual)) <-
+                Map.assocs $ Map.intersectionWith (,) cached actual
+            , weightCached /= weightActual
+            ]
+        unless (cached == actual)
+            updateAllIssueApprovals
+    pure $ length actual
+    where
+
+    handleDeleted = traverse_ $ deleteBy . UniqueMember target
+
+    handleAdded added =
+        insertMany_
+            [ StellarSigner
+                { stellarSignerTarget = target
+                , stellarSignerKey
+                , stellarSignerWeight
+                }
+            | (stellarSignerKey, stellarSignerWeight) <- Map.assocs added
+            ]
+
+    handleModified =
+        traverse_ \(key, weight) ->
+            updateWhere
+                [StellarSignerTarget ==. target, StellarSignerKey ==. key]
+                [StellarSignerWeight =. weight]
 
     db action = runSqlPool action connPool
 
@@ -103,3 +101,11 @@ stellarDataUpdater baseUrl connPool =
         issues <- selectList [] []
         for_ issues \(Entity issueId issue) ->
             Vote.updateIssueApproval issueId $ Just issue
+
+getAccount' :: BaseUrl -> Text -> IO Account
+getAccount' baseUrl address = do
+    manager <- newTlsManager
+    eResult <- runClientM (getAccount address) $ mkClientEnv manager baseUrl
+    case eResult of
+        Left err -> throwIO err
+        Right result -> pure result
