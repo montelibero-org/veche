f9d67d8cc6e1b6d1af6e6b6a01d169db79efaf0a
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-08-27 21:38:13 +0200

    veche-web:Workers.StellarUpdate: Add MTL holders cache

--- veche-web/src/Workers/StellarUpdate.hs
+++ veche-web/src/Workers/StellarUpdate.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
@@ -13,47 +14,56 @@ import Import hiding (cached)
 -- global
 import Control.Concurrent (threadDelay)
 import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
 import Database.Persist.Sql (ConnectionPool, runSqlPool)
 import Network.HTTP.Client.TLS (newTlsManager)
-import Servant.Client (BaseUrl, mkClientEnv, runClientM)
+import Servant.Client (BaseUrl, ClientEnv, ClientM, mkClientEnv, runClientM)
 import System.Random (randomRIO)
 
 -- project
-import Stellar.Horizon.Client (getAccount)
-import Stellar.Horizon.Types (Account (..), Signer (..),
+import Stellar.Horizon.Client (getAccount, getAllAccounts)
+import Stellar.Horizon.Types (Account (Account), Asset (Asset), Signer (Signer),
                               SignerType (Ed25519PublicKey))
+import Stellar.Horizon.Types qualified
 
 -- component
-import Genesis (mtlFund)
+import Genesis (mtlAsset, mtlFund)
 import Model.Vote qualified as Vote
 
 stellarDataUpdater :: BaseUrl -> ConnectionPool -> IO ()
-stellarDataUpdater baseUrl connPool =
+stellarDataUpdater baseUrl connPool = do
+    manager <- newTlsManager
+    let clientEnv = mkClientEnv manager baseUrl
     forever do
-        putStrLn "stellarDataUpdater: Updating MTL signers"
-        n <- updateMtlSignersCache baseUrl connPool mtlFund
-        putStrLn $ "stellarDataUpdater: Updated " <> tshow n <> " items"
+        do  putStrLn "stellarDataUpdater: Updating MTL signers"
+            n <- updateSignersCache clientEnv connPool mtlFund
+            putStrLn $
+                "stellarDataUpdater: Updated " <> tshow n <> " MTL signers"
+        do  putStrLn "stellarDataUpdater: Updating MTL holders"
+            n <- updateHoldersCache clientEnv connPool mtlAsset
+            putStrLn $
+                "stellarDataUpdater: Updated " <> tshow n <> " MTL signers"
         randomDelay
   where
     randomDelay = do
         delayMinutes <- randomRIO (1, 10)
         threadDelay $ delayMinutes * 60 * 1_000_000
 
-updateMtlSignersCache ::
-    BaseUrl ->
+updateSignersCache ::
+    ClientEnv ->
     ConnectionPool ->
     Text ->
     -- | Actual number of signers
     IO Int
-updateMtlSignersCache baseUrl connPool target = do
-    Account{signers} <- getAccount' baseUrl target
+updateSignersCache clientEnv connPool target = do
+    Account{signers} <- runClientM' clientEnv $ getAccount target
     let actual =
             Map.fromList
                 [ (key, weight)
                 | Signer{key, weight, type_ = Ed25519PublicKey} <- signers
                 , weight > 0
                 ]
-    db do
+    (`runSqlPool` connPool) do
         cached' <- selectList [StellarSignerTarget ==. target] []
         let cached =
                 Map.fromList
@@ -95,17 +105,50 @@ updateMtlSignersCache baseUrl connPool target = do
                 [StellarSignerTarget ==. target, StellarSignerKey ==. key]
                 [StellarSignerWeight =. weight]
 
-    db action = runSqlPool action connPool
-
     updateAllIssueApprovals = do
         issues <- selectList [] []
         for_ issues \(Entity issueId issue) ->
             Vote.updateIssueApproval issueId $ Just issue
 
-getAccount' :: BaseUrl -> Text -> IO Account
-getAccount' baseUrl address = do
-    manager <- newTlsManager
-    eResult <- runClientM (getAccount address) $ mkClientEnv manager baseUrl
-    case eResult of
-        Left err -> throwIO err
-        Right result -> pure result
+updateHoldersCache ::
+    ClientEnv ->
+    ConnectionPool ->
+    Asset ->
+    -- | Actual number of holders
+    IO Int
+updateHoldersCache clientEnv connPool asset@Asset{code, issuer} = do
+    holders <- runClientM' clientEnv $ getAllAccounts asset
+    let actual = Set.fromList [account_id | Account{account_id} <- holders]
+    (`runSqlPool` connPool) do
+        cached' <-
+            selectList
+                [ StellarHolderAssetCode    ==. code
+                , StellarHolderAssetIssuer  ==. issuer
+                ]
+                []
+        let cached =
+                Set.fromList
+                    [ stellarHolderKey
+                    | Entity _ StellarHolder{stellarHolderKey} <- cached'
+                    ]
+        handleDeleted $ cached \\ actual
+        handleAdded $ actual \\ cached
+    pure $ length actual
+
+  where
+
+    handleDeleted = traverse_ $ deleteBy . UniqueHolder code issuer
+
+    handleAdded added =
+        insertMany_
+            [ StellarHolder
+                { stellarHolderAssetCode    = code
+                , stellarHolderAssetIssuer  = issuer
+                , stellarHolderKey
+                }
+            | stellarHolderKey <- toList added
+            ]
+
+runClientM' :: ClientEnv -> ClientM a -> IO a
+runClientM' clientEnv action =
+    runClientM action clientEnv >>= either throwIO pure
