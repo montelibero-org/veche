35387f71a9ee8ae634cf445c4ff8584d5da9f8a5
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-08-27 20:53:39 +0200

    Add MTL asset info to Genesis

--- veche-web/src/Genesis.hs
+++ veche-web/src/Genesis.hs
@@ -4,5 +4,11 @@ module Genesis where
 
 import Import
 
+import Stellar.Horizon.Types (Asset (Asset))
+
+mtlAsset :: Asset
+mtlAsset =
+    Asset "MTL" "GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V"
+
 mtlFund :: Text
 mtlFund = "GDX23CPGMQ4LN55VGEDVFZPAJMAUEHSHAMJ2GMCU2ZSHN5QF4TMZYPIS"
