4ba88f12e39b552e99d8c875be8803a1fc0605cc
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-10-04 19:01:15 +0200

    Release 2022.10.4

--- veche-web/migration.sql
+++ veche-web/migration.sql
@@ -1,10 +1,12 @@
 DROP TABLE event;
 
-ALTER TABLE comment ADD COLUMN event_delivered BOOLEAN   NOT NULL DEFAULT FALSE;
+ALTER TABLE comment ADD COLUMN event_delivered BOOLEAN NOT NULL DEFAULT FALSE;
+UPDATE comment SET event_delivered = TRUE WHERE event_delivered = FALSE;
 
-ALTER TABLE issue   ADD COLUMN event_delivered BOOLEAN   NOT NULL DEFAULT FALSE;
+ALTER TABLE issue ADD COLUMN event_delivered BOOLEAN NOT NULL DEFAULT FALSE;
+UPDATE issue SET event_delivered = TRUE WHERE event_delivered = FALSE;
 
-ALTER TABLE request ADD COLUMN event_delivered BOOLEAN   NOT NULL DEFAULT FALSE;
-ALTER TABLE request ADD COLUMN created         TIMESTAMP NOT NULL DEFAULT 0;
-ALTER TABLE request DROP COLUMN delivered;
+ALTER TABLE request ADD COLUMN event_delivered BOOLEAN NOT NULL DEFAULT FALSE;
+UPDATE request SET event_delivered = TRUE WHERE event_delivered = FALSE;
+ALTER TABLE request ADD COLUMN created TIMESTAMP NOT NULL DEFAULT 0;
 UPDATE request SET created = CURRENT_TIMESTAMP WHERE created = 0;
