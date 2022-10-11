fc3ffd6e2a2b444b3e923ca448ae1a021270b653
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-10-11 16:47:35 +0200

    Replace rawSql with Esqueleto

--- veche-web/src/Model/Request.hs
+++ veche-web/src/Model/Request.hs
@@ -1,7 +1,7 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -13,14 +13,20 @@ module Model.Request (
     selectActiveByUser,
 ) where
 
+{- HLINT ignore "Fuse on/on" -}
+{- HLINT ignore "Redundant ^." -}
+
 import Import.NoFoundation
 
-import Database.Persist (toPersistValue)
-import Database.Persist.Sql (Single (Single), SqlBackend, rawSql)
+import Database.Esqueleto.Experimental (Value (Value), from, innerJoin, not_,
+                                        on, select, table, val, where_, (&&.),
+                                        (:&) ((:&)), (==.), (^.))
+import Database.Persist.Sql (SqlBackend)
 import Yesod.Persist (YesodPersist, YesodPersistBackend, runDB)
 
-import Model (Comment, Issue, IssueId, Request (Request), RequestId, User,
-              UserId)
+import Model (Comment,
+              EntityField (CommentId, IssueId, RequestId, Request_comment, Request_fulfilled, Request_issue, Request_user, UserId),
+              Issue, IssueId, Request (Request), RequestId, User, UserId)
 
 -- | A request for specific user
 data RequestMaterialized = RequestMaterialized
@@ -45,35 +51,40 @@ selectActiveByUser ::
 selectActiveByUser userId = do
     requests <-
         runDB $
-        rawSql
-            @(Single RequestId, Entity Issue, Entity Comment, Entity User)
-            "SELECT request.id, ??, ??, ??\
-            \ FROM request\
-            \ JOIN issue ON request.issue = issue.id\
-            \ JOIN comment ON request.comment = comment.id\
-            \ JOIN user ON request.user = user.id\
-            \ WHERE request.fulfilled = FALSE AND request.user = ?"
-            [toPersistValue userId]
+        select do
+            request :& issue :& comment :& user <- from $
+                table @Request
+                `innerJoin` table @Issue `on` (\(request :& issue) ->
+                    request ^. Request_issue ==. issue ^. IssueId)
+                `innerJoin` table @Comment `on` (\(request :& _ :& comment) ->
+                    request ^. Request_comment ==. comment ^. CommentId)
+                `innerJoin` table @User `on` \(request :& _ :& _ :& user) ->
+                    request ^. Request_user ==. user ^. UserId
+                    &&. request ^. Request_user ==. val userId
+            where_ $ not_ (request ^. Request_fulfilled)
+            pure (request ^. RequestId, issue, comment, user)
     pure
         [ RequestMaterialized{id, issue, comment, requestor}
-        | (Single id, issue, comment, requestor) <- requests
+        | (Value id, issue, comment, requestor) <- requests
         ]
 
 selectActiveByIssueAndUser ::
     MonadIO m => IssueId -> UserId -> SqlPersistT m [IssueRequestMaterialized]
 selectActiveByIssueAndUser issueId userId = do
     requests <-
-        rawSql
-            @(Single RequestId, Entity Comment, Entity User)
-            "SELECT request.id, ??, ??\
-            \ FROM request\
-            \ JOIN comment ON request.comment = comment.id\
-            \ JOIN user ON request.user = user.id\
-            \ WHERE   request.fulfilled = FALSE\
-                \ AND request.issue = ?\
-                \ AND request.user = ?"
-            [toPersistValue issueId, toPersistValue userId]
+        select do
+            request :& comment :& user <- from $
+                table @Request
+                `innerJoin` table @Comment `on` (\(request :& comment) ->
+                    request ^. Request_comment ==. comment ^. CommentId)
+                `innerJoin` table @User `on` \(request :& _ :& user) ->
+                    request ^. Request_user ==. user ^. UserId
+                    &&. request ^. Request_user ==. val userId
+            where_ $
+                not_ (request ^. Request_fulfilled)
+                &&. request ^. Request_issue ==. val issueId
+            pure (request ^. RequestId, comment, user)
     pure
         [ IssueRequestMaterialized{id, comment, requestor}
-        | (Single id, comment, requestor) <- requests
+        | (Value id, comment, requestor) <- requests
         ]
