a74ad2fb71ff55a3dc2c0976cf018aa2ddf9a04d
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-10-25 07:04:04 +0200

    Release 2022.10.25

--- veche-web/migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE escrow
-    ( sponsor_tx    VARCHAR NOT NULL PRIMARY KEY
-    , amount        VARCHAR NOT NULL
-    , asset         VARCHAR NOT NULL
-    , issue         INTEGER NOT NULL
-                    REFERENCES issue ON DELETE RESTRICT ON UPDATE RESTRICT
-    , sponsor       VARCHAR NOT NULL
-    , paid_tx       VARCHAR NULL
-    );
