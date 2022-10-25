7ad5cf383f2b4c3a34e4d8d4995328dee97bb553
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-10-25 07:03:41 +0200

    Fix table escrow

--- veche-web/migration.sql
+++ veche-web/migration.sql
@@ -1,10 +1,9 @@
 CREATE TABLE escrow
-    ( id            INTEGER PRIMARY KEY
+    ( sponsor_tx    VARCHAR NOT NULL PRIMARY KEY
     , amount        VARCHAR NOT NULL
     , asset         VARCHAR NOT NULL
     , issue         INTEGER NOT NULL
                     REFERENCES issue ON DELETE RESTRICT ON UPDATE RESTRICT
     , sponsor       VARCHAR NOT NULL
-    , sponsor_tx    VARCHAR NOT NULL
     , paid_tx       VARCHAR NULL
     );
