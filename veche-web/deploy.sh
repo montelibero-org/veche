b271fa800c233a1857c80864256e61fa382f6f54
Yuriy Syrovetskiy <git-orbi@cblp.su>
2022-12-01 14:50:45 +0100

    Fix deploy script

--- veche-web/deploy.sh
+++ veche-web/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
 
-stack --docker                                      \
-    exec --package=yesod-bin --                     \
+stack --docker --docker-image=fpco/stack-build:lts-19   \
+    exec --package=yesod-bin --                         \
     yesod keter
 
 scp veche.keter root@veche.montelibero.org:/opt/keter/incoming/
