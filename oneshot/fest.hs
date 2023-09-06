a2402067291a2ba872d7d2d08bd35d4d89dead32
Yuriy Syrovetskiy <git-orbi@cblp.su>
2023-09-06 13:19:40 +0200

    Add mtlfest tools

---
+++ oneshot/fest.hs
@@ -0,0 +1,54 @@
+{-# OPTIONS -Wno-missing-signatures #-}
+
+import Data.Function
+import Data.Map qualified as Map
+import Data.Text.IO qualified as Text
+import Data.Time
+import Named
+
+import Stellar.Simple hiding (issuer)
+
+main = do
+    client <- getPublicClient ! defaults
+    retryOnTimeout do
+
+        t <-
+            transactionBuilder (Address issuerPub)
+            -- & tx_feePerOp_guess
+            -- & op_setHomeDomain "assets.montelibero.org"
+            -- & op_payment token 20_000_e7
+            --     ! #destination (Address distributorPub)
+            --     ! #source (Address issuerPub)
+            -- & op_manageSellOffer
+            --     (#selling token)
+            --     (#unit eurmtl)
+            --     (#amount 10_000_e7)
+            --     (#price 1)
+            --     ! defaults
+            & op_setSigners
+                (Map.fromList [(Address addr, 1) | addr <- adminPubKeys])
+            & op_setThresholds (#low 1) (#med 1) (#high 1)
+            & op_setMasterWeight 0
+            & build client
+
+        -- secret <- Text.strip <$> Text.readFile "/tmp/secret"
+        let envelope = t & signWithSecret issuerSec -- & signWithSecret operatorSec
+        Text.putStrLn $ "envelope:\n" <> xdrSerializeBase64T envelope
+
+        putStrLn "submitting..."
+        print =<< getCurrentTime
+        submit envelope client >>= print
+
+issuerPub = "GCGWAPG6PKBMHEEAHRLTWHFCAGZTQZDOXDMWBUBCXHLQBSBNWFRYFEST"
+
+-- token = mkAsset "MTLFEST" issuerPub
+
+-- distributorPub = "GBJ4BPR6WESHII6TO4ZUQBB6NJD3NBTK5LISVNKXMPOMMYSLR5DOXMFD"
+
+adminPubKeys =
+    [ "GCPJUXPETZEIJNEAAEO2LGZIA6IQNNWGOHPNP4ZQECDS6IKKIGJO5LFV"
+    , "GCPT3X4FJBMUBR5AIB7SEUQX7HJ4XX3K4TNI2J7WIHMHMFGDMRRJJVWL"
+    ]
+
+-- eurmtl =
+--     mkAsset "EURMTL" "GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V"
